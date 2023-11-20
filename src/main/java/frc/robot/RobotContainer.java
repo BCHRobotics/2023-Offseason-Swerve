f9d8bdb21d59545383064bdb0b10c0e6c5802320
Stevon Anton <stevonssanta@gmail.com>
2023-11-20 18:24:24 -0500

    Added new command + Changed buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import frc.robot.Constants.OIConstants;
 import frc.robot.commands.Autos;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
@@ -57,10 +58,19 @@ public class RobotContainer {
    * {@link JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(m_driverController, Button.kR1.value)
+    // Break Command (RT)
+    new JoystickButton(m_driverController, Button.kR2.value)
         .whileTrue(new RunCommand(
             () -> m_robotDrive.setX(),
             m_robotDrive));
+
+    // Reset Gyro Command (Y)
+    new JoystickButton(m_driverController, Button.kTriangle.value)
+        .onTrue(new InstantCommand(
+            () -> m_robotDrive.zeroHeading(),
+            m_robotDrive));
+
+        
   }
 
     /**
