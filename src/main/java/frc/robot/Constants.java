8e9d990c0f2565dc79079fae1a906dd25cb045ae
Stevon Anton <stevonssanta@gmail.com>
2023-12-15 12:17:10 -0500

    Tested auto

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -130,8 +130,8 @@ public final class Constants {
   }
 
   public static final class AutoConstants {
-    public static final double kMaxSpeedMetersPerSecond = 3;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kMaxSpeedMetersPerSecond = 1.6;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 1.5;
     public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
     public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
 
--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -38,10 +38,19 @@ public class Autos {
     Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
         // Start at the origin facing the +X direction
         new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-        // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3, 0, new Rotation2d(0)),
+        // Pass through these eight  interior waypoints, making an '8' curve path
+        List.of(
+          new Translation2d(0.25, 0.75),
+          new Translation2d(1, 1), 
+          new Translation2d(2, 0),
+          new Translation2d(3, -1),
+          new Translation2d(4, 0),
+          new Translation2d(3, 1),
+          new Translation2d(2, 0),
+          new Translation2d(1, -1)
+        ),
+        // End at the beginning.
+        new Pose2d(0, 0, new Rotation2d(0)),
         config);
 
     var thetaController = new ProfiledPIDController(
